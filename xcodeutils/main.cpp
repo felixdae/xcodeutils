b84fd490a08ea16a793b413dc5d55fca21a5556e
Yingshen Yu <jonnyyu@192.168.1.5>
2011-07-29 21:15:55 +0800

    Initial Commit

---
+++ xcodeutils/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  xcodeutils
+//
+//  Created by Yu Yingshen on 11-7-29.
+//  Copyright 2011年 HOME. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
